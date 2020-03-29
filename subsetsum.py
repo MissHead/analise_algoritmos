4eb0594b4a2437d457609bea91b247c415c3d96b
MissHead <izabela.head@gmail.com>
2020-03-29 10:14:32 -0300

    definindo função subsetsum

--- subsetsum.py
+++ subsetsum.py
@@ -1,1 +1,7 @@
 def subsetsum(soma, lista, numero)
+    if soma != 0 and numero == 0:
+        return 0
+    if soma == 0
+        return 1
+    if lista[numero - 1] > soma:
+        return subsetsum(soma, lista, numero - 1)    
