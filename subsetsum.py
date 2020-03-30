dc4e17121500edff8f584f344fd4a662e7fcf1b5
MissHead <izabela.head@gmail.com>
2020-03-30 11:50:53 -0300

    Resultado booleano para SubsetSum (Soma de subconjuntos)

--- subsetsum.py
+++ subsetsum.py
@@ -1,10 +1,12 @@
 def subsetsum(soma, lista, tamanho):
     if soma != 0 and tamanho == 0:
-        return 0
+        return False
     if soma == 0:
-        return 1
+        return True
     if lista[tamanho - 1] > soma:
-        return subsetsum(soma, lista, tamanho - 1) 
+        pass
+    resultado = subsetsum(soma, lista, tamanho - 1) or subsetsum(soma - lista[tamanho - 1], lista, tamanho - 1) 
+    return resultado
 
 entrada = input()
 lista = entrada.split()
@@ -12,5 +14,5 @@ lista = [int(valor) for valor in lista]
 soma = input()
 soma = int(soma)
 tamanho = len(lista)
-subsetsum(soma, lista, tamanho)
-print(lista, soma, tamanho)
+resultado = subsetsum(soma, lista, tamanho)
+print('Subconjunto tem como soma: ' + str(soma)) if resultado else print('Não há subconjunto que satisfaça a soma: ' + str(soma))
