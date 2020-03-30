d04ef323eb58ae10bd9b88bedc190bf017cf2f07
MissHead <izabela.head@gmail.com>
2020-03-30 14:06:44 -0300

    subsetsum retornando lista do primeiro subconjunto encontrado correspondente à soma

--- subsetsum.py
+++ subsetsum.py
@@ -1,18 +1,23 @@
-def subsetsum(soma, conjunto, tamanho):
-    if soma != 0 and tamanho == 0:
+def subsetsum(soma, conjunto):
+    if soma == 0 or soma < 1:
         return False
-    if soma == 0:
-        return True
-    if conjunto[tamanho - 1] > soma:
-        pass
-    resultado = subsetsum(soma, conjunto, tamanho - 1) or subsetsum(soma - conjunto[tamanho - 1], conjunto, tamanho - 1) 
-    return resultado
-
+    elif len(conjunto) == 0:
+        return False
+    else:
+        if conjunto[0] == soma:
+            return [conjunto[0]]
+        else:
+            subconjunto = subsetsum((soma - conjunto[0]), conjunto[1:]) 
+            if subconjunto:
+                return [conjunto[0]] + subconjunto
+            else:
+                return subsetsum(soma, conjunto[1:])
+    
 entrada = input()
 conjunto = entrada.split()
 conjunto = [int(valor) for valor in conjunto]
 soma = input()
 soma = int(soma)
 tamanho = len(conjunto)
-resultado = subsetsum(soma, conjunto, tamanho)
-print('Subconjunto tem como soma: ' + str(soma)) if resultado else print('Não há subconjunto que satisfaça a soma: ' + str(soma))
+resultado = subsetsum(soma, conjunto)
+print('O Subconjunto: ' + str(resultado) + ' tem como soma: ' + str(soma)) if resultado else print('Não há subconjunto que satisfaça a soma: ' + str(soma))
